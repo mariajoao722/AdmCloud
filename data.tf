0f5a133c94223d893267da632aabdb0d41f601f6
monica-a-araujo <up202005209@up.pt>
2024-04-10 13:46:26 +0100

    ssh - update

--- data.tf
+++
@@ -1,3 +0,0 @@
-data "tls_public_key" "public_key_data" {
-  private_key_pem = tls_private_key.ssh_key_pk.private_key_pem
-}
--- outputs.tf
+++ outputs.tf
@@ -8,16 +8,6 @@ output "instance_id2" {
   value       = google_compute_instance.teste2.instance_id
 }
 
-output "private_key" {
-  value = tls_private_key.ssh_key_pk.private_key_pem
-  sensitive=true
-}
-
-output "public_key" {
-  value = "${data.tls_public_key.public_key_data.public_key_pem}"
-  sensitive = true
-}
-
 output "public_ip_teste1" {
   value = google_compute_instance.teste1.network_interface[0].access_config[0].nat_ip
 }
